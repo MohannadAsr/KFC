cf5e827c645492cfb3d54e0b3d98139c990742cf
Mohannad Adnan Alassar <mohannad.alassar@hotmail.com>
2024-02-01 00:34:07 +0300

    add and correct the reset funcs

--- controllers/resetController.js
+++ controllers/resetController.js
@@ -5,21 +5,13 @@ const { Transactions } = require('../models');
 const { Users } = require('../models');
 
 exports.resetDatabase = catchAsync(async (req, res) => {
-  const t = await sequelize.transaction();
-
   // Use the destroy method with the transaction option
-  await Users.destroy({ where: {}, truncate: false, transaction: t });
-  await Products.destroy({ where: {}, truncate: false, transaction: t });
-  await Transactions.destroy({ where: {}, truncate: false, transaction: t });
+  await Users.destroy({ where: {}, truncate: false });
+  await Products.destroy({ where: {}, truncate: false });
+  await Transactions.destroy({ where: {}, truncate: false });
 
   // Create a default user within the transaction
-  await Users.create(
-    { name: 'SuperAdmin', code: 12345, role: 'admin' },
-    { transaction: t }
-  );
-
-  // Commit the transaction if everything is successful
-  await t.commit();
+  await Users.create({ name: 'SuperAdmin', code: 12345, role: 'admin' });
 
   res.json({ status: 200, data: 'Data reset successfully' });
 });
